0029da1a15f2569b61f8ca76eee7a5939931447e
mateus magalhaes <mateusfcmagalhaes@gmail.com>
2024-11-01 20:45:38 -0300

    tiro funcional

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    Vector3 velocity;
+
+    float speed = 20;
+
+    float gravity = 1.5f;
+    float wind    = 1.5f;
+
+    BoxCollider collider;
+
+    void Start() {
+
+        collider = GetComponent<BoxCollider>();
+
+    }
+
+    void Update() {
+
+        velocity += gravity * Vector3.down * Time.deltaTime;
+        velocity += wind * transform.right * Time.deltaTime;
+
+        transform.position += velocity * Time.deltaTime;
+
+        Collider[] colliders = Physics.OverlapBox(transform.position, collider.size / 2, transform.rotation, LayerMask.GetMask("hittable"));
+
+        if(colliders.Length > 0) {
+
+            IShotHit hitted = colliders[0].GetComponent<IShotHit>();
+            if(hitted != null) {
+                hitted.Hit(velocity.normalized);
+            }
+
+            Destroy(gameObject);
+
+        }
+
+
+    }
+
+    public void SetDirection(Vector3 direction) {
+
+        velocity = direction * speed;
+
+    }
+
+}
+
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+
+public class Gun : MonoBehaviour {
+
+    public Transform bulletPrefab;
+
+    Animator    animator;
+    AudioSource audioSource;
+
+    FirstPersonCamera fpsCam;
+
+    ShotEffects shootEffects;
+
+    Transform shotSpawn;
+    Transform shellSpawn;
+
+    string gunName = "Sniper";
+
+    private void Awake() {
+
+        animator    = transform.Find(gunName).GetComponent<Animator>();
+        audioSource = transform.GetComponent<AudioSource>();
+
+        fpsCam     = transform.parent.GetComponent<FirstPersonCamera>();
+        shotSpawn  = transform.Find("shotSpawn");
+        shellSpawn = transform.Find("shellSpawn");
+
+        shootEffects = GetComponent<ShotEffects>();
+
+    }
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+        if(Input.GetKeyDown(KeyCode.Mouse0)) {
+
+            animator.Play("Shoot", -1, 0);
+
+            audioSource.Play();
+
+            shootEffects.MuzzleFlash(shotSpawn.position, shotSpawn.rotation);
+            shootEffects.Shell(shellSpawn.position, shellSpawn.rotation);
+
+            // implementação do tiro vem aqui....
+            // ShootRaycast();
+            ShootBullet();
+
+        }
+
+    }
+
+    void ShootBullet() {
+
+        Transform bulletObj = Instantiate(bulletPrefab, shotSpawn.position, shotSpawn.rotation);
+        Destroy(bulletObj.gameObject, 10f);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.GetForwardDirection(), out hitInfo, Mathf.Infinity, LayerMask.GetMask("hittable"))) {
+
+            bulletObj.GetComponent<Bullet>().SetDirection((hitInfo.point - shotSpawn.position).normalized);
+
+        } else {
+            bulletObj.GetComponent<Bullet>().SetDirection(fpsCam.GetForwardDirection());
+        }
+
+    }
+
+
+    void ShootRaycast() {
+
+        RaycastHit hitInfo;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.GetForwardDirection(), out hitInfo, Mathf.Infinity, LayerMask.GetMask("hittable"))) {
+
+            IShotHit hitted = hitInfo.transform.GetComponent<IShotHit>();
+            if(hitted != null) {
+
+                hitted.Hit(fpsCam.GetForwardDirection());
+
+            }
+        }
+    }
+
+
+   
+
+}
